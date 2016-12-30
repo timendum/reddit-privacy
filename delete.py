18d603b444338b808251275597176050bc4122f3
Timendum <timedum@gmail.com>
2016-12-30 12:07:50 +0100

    praw upgrade to 4.0

--- delete.py
+++ delete.py
@@ -9,9 +9,8 @@ logger.addHandler(logging.StreamHandler())
 REDDIT_HARDLIMIT = 1000 - 1
 
 def main(days_limit=365, number_limit=None):
-	r = praw.Reddit(user_agent='python:reddit-privacy-delete:0.1 (by /u/timendum)')
-	r.login(disable_warning=True)
-	user = r.user
+	r = praw.Reddit(user_agent='python:reddit-privacy-delete:0.2 (by /u/timendum)')
+	user = r.user.me()
 	# Limits
 	if number_limit is not None and number_limit > REDDIT_HARDLIMIT:
 		number_limit = REDDIT_HARDLIMIT
@@ -20,27 +19,32 @@ def main(days_limit=365, number_limit=None):
 	created_limit = datetime.now() - timedelta(days=days_limit)
 	print('Criteria: user %s\'s submissions and comments older then %s days OR then the %dth content' % (user.name, created_limit.isoformat(), number_limit))
 	# Submission
-	submitted = user.get_submitted(sort='new', limit=None)
+	submitted = user.submissions.new(limit=None)
 	check(submitted, number_limit, created_limit)
 	# Comments
-	comments = user.get_comments(sort='new', limit=None)
+	comments = user.comments.new(limit=None)
 	check(comments, number_limit, created_limit)
 
 def check(contents, number_limit, created_limit):
 	count = 0
+	to_delete = []
 	for e in contents:
 		time = e.created
 		submitted = datetime.fromtimestamp(time)
 		if submitted < created_limit:
-			print('Deleting ' + e.permalink)
-			e.delete()
+			to_delete.append(e)
 			continue
 
 		if count >= number_limit:
+			to_delete.append(e)
+			continue
+		count += 1
+	for e in to_delete:
+		try:
 			print('Deleting ' + e.permalink)
-			e.delete()
-		else:
-			count += 1
+		except TypeError:
+			print('Deleting ' + e.permalink(fast=True))
+		e.delete()
 
 if __name__ == "__main__":
 	import sys
